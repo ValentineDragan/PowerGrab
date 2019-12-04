e0998e972fa2faa9028507871537203c209b3459
ValntinDragan <draganvalentinadrian@gmail.com>
2019-12-04 00:21:21 +0000

    Refactored DroneLogic

--- src/main/java/uk/ac/ed/inf/powergrab/DroneLogic.java
+++ src/main/java/uk/ac/ed/inf/powergrab/DroneLogic.java
@@ -2,7 +2,6 @@ package uk.ac.ed.inf.powergrab;
 
 public interface DroneLogic {
 	
-	// This method must return the Drone's chosen next move.
-	// This method will be called by the GameManager each turn.
+	// This method must return the Drone's chosen next move, based on its current Position.
 	public Direction getNextMove(Position currentPos);
 }
